817a6c536c3a36b2979165f1ba99bf03d376dd4c
NolWag <NolanDubyu@gmail.com>
2018-06-18 03:18:19 -0500

    updates

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './components/css/styles.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Views
+import App from './components/App';
+import Home from './components/Home';
+import CurrentWeather from './components/CurrentWeather';
+import ErrorDisplay from './components/ErrorDisplay';
+
+ReactDOM.render(
+  <Router>
+    <App>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/current-weather' component={CurrentWeather}/>
+      <Route exact path='/error' component={ErrorDisplay}/>
+    </App>
+  </Router>
+  , document.getElementById('root'));
